34cdf9ad00e457ea264fb712b47f83a1c4000466
Ubuntu <ubuntu@ip-172-31-44-116.ap-northeast-1.compute.internal>
2022-10-28 08:51:52 +0000

    弁当半額実装

--- practice.rb
+++ practice.rb
@@ -1,15 +1,10 @@
-=begin 
-入力 降水確率
-処理 降水確率によって天気を分ける
-出力 降水確率に合わせた天気予報を出す
+# def masa(a ,product_numbers)
+    
+#     puts a 
+#     product_numbers.each do |b|
+#         puts b
+#     end 
+# end 
 
-=end 
-n = gets.to_i
+# masa(7,[1,2,3,4])
 
-if n <= 30
-  puts "sunny"
-elsif n >= 31 &&  n <= 70
-   puts "cloudy"
-else puts "rainy"
- end 
-   
--- test.rb
+++ test.rb
@@ -10,137 +10,24 @@
 ｱｳﾄﾌﾟｯﾄ(出力)
 =end 
 
-
-  
-  
-
-
-
-# def calc(current_time, *product_numbers)
-#     # 定数
-#   onion_discounted = 50 #玉ねぎ3つ
-#   onion_more_discounted = 100  #玉ねぎ5つ
-    # bento_drink_discounted = 20    #弁当と飲み物が選ばれた時の割引
-  
-  
-#   # 商品一覧
-#   list_items = [
-#     ["玉ねぎ",100], #1
-#     ["人参",150], #2
-#     ["りんご",200], #3
-#     ["ぶどう",350], #4
-#     ["牛乳",180], #5
-#     ["卵",220], #6
-#     ["唐揚げ弁当",440], #7
-#     ["のり弁",380], #8
-#     ["お茶",80], #9
-#     ["ｺｰﾋｰ",100], #10
-            
-    # ]
-    # 商品の金額の合計(税抜)
-    # price = 0
-    
-    # if current_time == 9..19 and product_numbers.count(1)  >= 5
-    # for i in product_numbers 
-    # price += list_items[i - 1][1]
-    # end 
-    #   price -=  onion_more_discounted 
-    
-    # elsif current_time == 9..19 and 
-    #   product_numbers.count(1) < 5 
-    #   for i in product_numbers 
-    #   price += list_items[i - 1][1]
-    # end 
-    #   price -= discounted
-    
-   
-    
-    # 弁当半額
-    # elsif current_time == 20..23 and  product_numbers.count(7) >= 1 || product_numbers.count(8) >= 1
-    # for i in product_numbers
-    #   if i == 7 
-    #   price += list_items[6][1] / 2
-    #   elsif i == 8
-    #   price += list_items[7][1] / 2
-    #   else
-    #   price += list_items[i - 1][1]
-    # end 
-    # end 
-    # end 
-    
-    
-    # 弁当と飲み物を一緒に買うと20円引きにする
-  
-  # 商品の金額の合計(税込)
-#   tatal = price * 1.1
-#   puts tatal.floor
-  
-# end 
- 
-# calc(20, 8)
-
-# a = [ "a", "b", "c" ] 
-# b = [ "a", "b", "c" ,"d","e"] 
-# puts b.permutation(a.size).include?(a) 
-
-
-=begin 
-①弁当と飲み物の配列を作成する
-②product_numbersの配列から弁当と飲み物に該当するproduct_numbersを順番に抜き出す
-③①の弁当の配列と飲み物の配列にそのproduct_numberがあるかどうかを調べる
-③20円引く
-=end 
-
-
-
-# def calc(current_time, *product_numbers)
-     
-     
-# # # 商品一覧
-#   list_items = [
-#     ["玉ねぎ",100], #1
-#     ["人参",150], #2
-#     ["りんご",200], #3
-#     ["ぶどう",350], #4
-#     ["牛乳",180], #5
-#     ["卵",220], #6
-#     ["唐揚げ弁当",440], #7
-#     ["のり弁",380], #8
-#     ["お茶",80], #9
-#     ["ｺｰﾋｰ",10]  #10
-# ]
-# bevarages = [5,9,10] #牛乳､お茶､ｺｰﾋｰ
-# bento = [7,8] #唐揚げ弁当､のり弁当
-# bevarages.each do |bevarage|
-# bento.each do |bento|
-#   if product_numbers.include?(bevarage) &&
-#   product_numbers.include?(bento)
-
-#     price -= combo_discounted end 
-# else 
-# for i in product_numbers 
-#     combo_discounted = 20
-#     price = 0
-#     price += list_items[i - 1][1]
-#   end 
-# end
-# end 
-# end 
-
-# tatal = price * 1.1
-#   puts tatal.floor
-# end 
-
-# calc(19, 5,7)
-
 #ｽｰﾊﾟｰで買った商品の合計金額を計算するﾌﾟﾛｸﾞﾗﾑ
-def calc(current_time, *product_numbers)#購入時刻と商品番号
+def calc(current_time, product_numbers)#購入時刻と商品番号
   
+  # 定数と変数
   #税抜価格
   without_tax = 0
   #税込価格
   total_tax_included = 0
   
+   #飲み物
+ drinks = [5,8,9]
+ 
+ #弁当 
+ bentos = [7,8]
+ 
+ three_onion_discounted = 50 #玉ねぎ3つ
+ five_onion_discounted = 100  #玉ねぎ5つ
+ bento_drink_discounted = 20 #弁当と飲み物をセットで買うと２０円引き
  
   
   #商品一覧
@@ -157,40 +44,72 @@ items_list = [
   ["ｺｰﾋｰ",100],#商品番号10
  ]
  
- #飲み物
- drinks = [5,8,9]
- 
- #弁当 
- bentos = [7,8]
- 
-#入力された商品の税抜き価格を計算するメソッド
-def chosen_items 
-  for i in product_numbers
-  without_tax += items_list[i - 1][1]
-end 
-end 
-
-#注文された商品番号に弁当と飲み物が含まれているとき20円びきをする税抜価格を計算しています｡
+ #注文された商品番号に弁当と飲み物が含まれているとき20円びきをする税抜価格を計算しています｡
+def bentos_drink_set
 if product_numbers.any? {|p| drinks.include?(p)} && product_numbers.any? {|a| bentos.include?(a)} 
 for i in product_numbers
   without_tax += items_list[i - 1][1]
 end 
-without_tax -= 20
+without_tax -= bento_drink_discounted
 else 
   for i in product_numbers
   without_tax += items_list[i - 1][1]
 end 
- 
+end 
 end 
 
+  
 
+
+  # 玉ねぎ３個の割引
+  without_tax -= three_onion_discounted if product_numbers.count(1) == 3
+  
+  
+  # 玉ねぎ５つの割引
+  without_tax -= five_onion_discounted if product_numbers.count(1) == 5
+  
+  # 営業時間によって割引があるかないかが変わるプログラム
+  case current_time
+  
+  when 9..19
+    # 玉ねぎ３個の割引
+  without_tax -= three_onion_discounted if product_numbers.count(1) == 3
+   # 玉ねぎ５つの割引
+  without_tax -= five_onion_discounted if product_numbers.count(1) == 5
+   
+    # 弁当半額セール
+  when 20..23
+    # 弁当半額
+if bentos_array = product_numbers.select {|number| number == 8 || 7}
+  product_numbers.delete(7)
+  product_numbers.delete(8)
+  bentos_array.each do |i|
+    total_tax_included += items_list[i - 1][1] / 2
+  end 
+  product_numbers.each do |i|
+    total_tax_included += items_list[i - 1][1]
+  end 
+  puts total_tax_included
+else
+     product_numbers.each do |i|
+    total_tax_included += items_list[i - 1][1]
+  end 
+
+     # 玉ねぎ３個の割引
+     without_tax -= three_onion_discounted if product_numbers.count(1) == 3
+   # 玉ねぎ５つの割引
+  without_tax -= five_onion_discounted if product_numbers.count(1) == 5
+ end 
+    
+  
+# 税込み価格の計算
 total_tax_included = without_tax * 1.1
 puts total_tax_included.floor
-  
 
 end
 
-
-calc(20,5,7)
-
 #第一引数に購入時刻､第二引数以降に商品番号が渡されます
+calc(9[1,1,1])
+
+
+
